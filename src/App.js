27b33b118455f3a1e89057d7b4ae6c351ef6413b
RenuJaishankar <Renu.Jaishankar@yahoo.com>
2020-06-02 13:10:36 -0400

    React files added

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MyComponent from './MyComponent';
+
+
+function App() {
+  return (
+    <div>
+      <h1>This is my React app.</h1>
+      <MyComponent/>
+    </div>
+   
+  );
+}
+
+export default App;
---
+++ src/MyComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const MyComponent = () => {
+  let myName = "Renu"
+  let myHobby = "I like to code."
+  return(
+  <div>
+    <h1>My name is {myName}.</h1>
+  <h2>Hi {myHobby}.</h2>
+
+  </div>
+
+  )
+   
+}
+
+export default MyComponent
